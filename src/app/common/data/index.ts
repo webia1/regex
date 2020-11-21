4876b6e9dfff12ef72754f931fa8092eca8669bd
@webia1 <@webia1>
2020-11-21 17:25:22 +0100

    Beispiel Link

--- src/app/common/data/index.ts
+++ src/app/common/data/index.ts
@@ -17,13 +17,13 @@ export const RegexExamples = [
   {
     regex: '/[\'"][^\'"]*[\'"]/',
     desc:
-      'Alles unter single/double quote, wobei beginnende und abschließende nicht gleich sind. Nächstes Beispiel löst das Problem.',
+      '<div>Alles unter single/double quote, wobei beginnende und abschließende nicht gleich sind. Nächstes Beispiel löst das Problem.</div><div><strong>Wichtig:</strong> Das Dachzeichen "^" innerhalb der eckigen Klammern bedeutet Negierung (d.h. darf nicht enthalten sein). </div>',
     url: '',
   },
   {
     regex: '/([\'"])[^\'"]*\\1/',
     desc:
-      '<div>Alles unter single/double quote, wobei beginnende und abschließende gleich sind</div><div>"<strong>\\1</strong>" ist eine Referenz auf den ersten Klammerinhalt. D.h. was immer dort (zwischen dem ersten Klammerpaar vorkam ) vorkam, muss auch hier ( wo "\\1" steht) vorkommen.</div> <div><strong>Wichtig:</strong> Das Dachzeichen "^" innerhalb der eckigen Klammern bedeutet Negierung. </div> ',
+      '<div>Alles unter single/double quote, wobei beginnende und abschließende gleich sind</div><div>"<strong>\\1</strong>" ist eine Referenz auf den ersten Klammerinhalt. D.h. was immer dort (zwischen dem ersten Klammerpaar vorkam ) vorkam, muss auch hier ( wo "\\1" steht) vorkommen.</div>',
     url: 'https://regex101.com/r/J6ZTfl/1/',
   },
 ];
